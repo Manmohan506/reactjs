16d289176d7656b39ca6e748adddb49319234312
Manmohan506 <manmohan@zelarsoft.com>
2021-09-22 12:47:28 +0530

    dd

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,16 +1,20 @@
-// const  createProxyMiddleware  = require('http-proxy-middleware');
+//const  createProxyMiddleware  = require('http-proxy-middleware');
+//const url = process.env.CONTAINER_NAME
+//module.exports = function(app) {
+  //app.use(
+    //createProxyMiddleware({
+      //target: url,
+      //changeOrigin: true,
+    //})
+  //);
+//};
+
 const url = process.env.CONTAINER_NAME;
+
 const proxy = require('http-proxy-middleware');
-// module.exports = router;
-// module.exports = function(app) {
-//   app.use(
-//     '/api/customers',
-//     createProxyMiddleware({
-//       target: url,
-//       changeOrigin: true,
-//     })
-//   );
-// };
+
 module.exports = function(app) {
+  
   app.use(proxy('/api/customers', { target: url }))
+
 }
